114c7ce344e3d65077e20bd366aec9a63995ee15
HyeonGi Min <hyeoni1995@gmail.com>
2024-09-28 23:35:55 +0900

    Api Gateway 구성
    yml 설정 방법 및 Config 구현방법 각각 구성
    선호하는 방식으로 진행하면 됨

---
+++ src/main/java/com/wimp/apigateway/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package com.wimp.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // first-service
+//                .route(r -> r.path("/first-service/**")
+//                        .uri("http://localhost:8081"))
+                // second-service
+//                .route(r -> r.path("/second-service/**")
+//                        .filters(f -> f.addResponseHeader("second-request", "second-request-header")
+//                                .addResponseHeader("second-response", "second-response-header"))
+//                        .uri("http://localhost:8082"))
+                // redirect to
+                .route(r -> r.path("/servicediscovery")
+                        .filters(f -> f
+                                .setStatus(HttpStatus.FOUND)  // 302 상태 코드로 설정
+                                .redirect(302, "http://oci-hyeoni1995.duckdns.org:8761"))  // 리다이렉트할 URL
+                        .uri("http://oci-hyeoni1995.duckdns.org:8761"))  // uri는 필요 없음
+                .build();
+    }
+}
