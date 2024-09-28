5d3621e9968409d55af03ed4e9eb5f4c53831a68
HyeonGi Min <hyeoni1995@gmail.com>
2024-09-28 22:56:57 +0900

    Initialize Spring Boot -기본 Spring Project Init

---
+++ src/main/java/com/wimp/apigateway/WimpApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wimp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimpApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimpApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wimp/apigateway/WimpApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimp.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimpApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
